ca5926696009be8a628590ca732974f4f750eb50
iamroot404 <tuvareagan253@.com>
2024-01-11 17:28:34 +0300

    ui

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { DisplayDifficulty } from "./components/DisplayDifficulty/DisplayDifficulty";
+
+export function App(){
+    return <div>
+        <DisplayDifficulty difficulty = {"Low"}/>
+    </div>
+}
